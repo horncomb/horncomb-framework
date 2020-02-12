35551ec1521a0d133f6e1aaba1277f61fe5ed125
chenry <chenry@newland.com.cn>
2020-02-12 09:30:06 +0800

    增加退出登录跨域

--- src/main/java/cn/horncomb/framework/security/shiro/RestLogoutFilter.java
+++ src/main/java/cn/horncomb/framework/security/shiro/RestLogoutFilter.java
@@ -1,6 +1,7 @@
 package cn.horncomb.framework.security.shiro;
 
 import org.apache.shiro.web.filter.authc.LogoutFilter;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -21,9 +22,29 @@ public class RestLogoutFilter extends LogoutFilter {
     @Override
     protected void issueRedirect(ServletRequest request, ServletResponse response, String redirectUrl) throws Exception {
         HttpServletResponse resp = (HttpServletResponse) response;
+        prepareResponse(HttpStatus.OK,request,response);
         if (this.logoutResponseBuilder != null)
             this.logoutResponseBuilder.writeSuccessResponse((HttpServletRequest) request, resp);
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.flushBuffer();
+    }
+
+    private HttpServletResponse prepareResponse(HttpStatus httpStatus, ServletRequest request, ServletResponse
+            response) {
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpServletRequest requ = (HttpServletRequest) request;
+        //跨域的header设置
+        resp.setHeader("Access-control-Allow-Origin", requ.getHeader("Origin"));
+        resp.setHeader("Access-Control-Allow-Methods", requ.getMethod());
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+        resp.setHeader("Access-Control-Max-Age", "3600");
+        resp.setHeader("Access-Control-Allow-Headers", requ.getHeader("Access-Control-Request-Headers"));
+        resp.setHeader("Access-Control-Expose-Headers","Authorization");
+        //防止乱码，适用于传输JSON数据
+        resp.setHeader("Content-Type","application/json;charset=UTF-8");
+        resp.setCharacterEncoding(request.getCharacterEncoding());
+        resp.setStatus(httpStatus.value());
+//        resp.setContentType(request.getContentType());
+        return resp;
     }
 }
