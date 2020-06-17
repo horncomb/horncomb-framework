69d383a6edac5ea9782d8472e5a6ae94d4b4ed61
huangzhb <331820688@qq.com>
2020-06-17 19:06:09 +0800

    Revert "调整路由取数"
    This reverts commit 1e9be5a765982bbed085ea63619733068c65423f.

--- src/main/java/cn/horncomb/framework/security/RoleService.java
+++ src/main/java/cn/horncomb/framework/security/RoleService.java
@@ -1,12 +1,7 @@
 package cn.horncomb.framework.security;
 
-import java.util.List;
-
 public interface RoleService {
 
-    Role[] getRolesByUserId(Object userId);
+    Role[] getById(Object userId);
 
-    List<String> getPermesByUserId(Object userId);
-
-    List<String> getDataScopesByUserId(Object userId);
 }
--- src/main/java/cn/horncomb/framework/security/shiro/DefaultUserRealm.java
+++ src/main/java/cn/horncomb/framework/security/shiro/DefaultUserRealm.java
@@ -67,7 +67,7 @@ public class DefaultUserRealm extends AuthorizingRealm {
         //loginType: ‘0’:用户端登录, ‘1’:机构端登录，‘2’：管理平台登录
         if("1".equals(upToken.getLoginType())||"2".equals(upToken.getLoginType())){
             //机构端和管理平台登录权限：机构用户
-            Role[] roleArr = roleService.getRolesByUserId((Long)account.getId());
+            Role[] roleArr = roleService.getById((Long)account.getId());
             boolean loginFlag = false;
             if(roleArr!=null&&roleArr.length>0){
                 loginFlag = true;
